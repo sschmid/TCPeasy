2814a3c28ad40d254d0a97bb5ab189a13b65fb8e
Simon Schmid <mail@sschmid.com>
2022-10-06 14:20:47 +0200

    Initial commit

---
+++ src/TCPeasy.Cli/src/Program.cs
@@ -0,0 +1,12 @@
+﻿using DesperateDevs.Cli.Utils;
+
+namespace TCPeasy.Cli
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            new CliProgram("TCPeasy", typeof(HelpCommand), args).Run();
+        }
+    }
+}
